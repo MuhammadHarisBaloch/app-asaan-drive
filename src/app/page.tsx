495622f58680794cc623c66136e0f645675a9322
Muhammad Anas <anasgulkhaskheli@gmail.com>
2025-05-31 17:06:19 +0500

    [FEAT] Header  Design  Complete

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,95 +1,28 @@
-import Image from "next/image";
-import styles from "./page.module.css";
+import { AppShell, AppShellHeader, AppShellMain,Button,Flex,Group,Image,Paper,Text, UnstyledButton } from "@mantine/core";
 
 export default function Home() {
-  return (
-    <div className={styles.page}>
-      <main className={styles.main}>
-        <Image
-          className={styles.logo}
-          src="/next.svg"
-          alt="Next.js logo"
-          width={180}
-          height={38}
-          priority
-        />
-        <ol>
-          <li>
-            Get started by editing <code>src/app/page.tsx</code>.
-          </li>
-          <li>Save and see your changes instantly.</li>
-        </ol>
-
-        <div className={styles.ctas}>
-          <a
-            className={styles.primary}
-            href="https://vercel.com/new?utm_source=create-next-app&utm_medium=appdir-template&utm_campaign=create-next-app"
-            target="_blank"
-            rel="noopener noreferrer"
-          >
-            <Image
-              className={styles.logo}
-              src="/vercel.svg"
-              alt="Vercel logomark"
-              width={20}
-              height={20}
-            />
-            Deploy now
-          </a>
-          <a
-            href="https://nextjs.org/docs?utm_source=create-next-app&utm_medium=appdir-template&utm_campaign=create-next-app"
-            target="_blank"
-            rel="noopener noreferrer"
-            className={styles.secondary}
-          >
-            Read our docs
-          </a>
-        </div>
-      </main>
-      <footer className={styles.footer}>
-        <a
-          href="https://nextjs.org/learn?utm_source=create-next-app&utm_medium=appdir-template&utm_campaign=create-next-app"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          <Image
-            aria-hidden
-            src="/file.svg"
-            alt="File icon"
-            width={16}
-            height={16}
-          />
-          Learn
-        </a>
-        <a
-          href="https://vercel.com/templates?framework=next.js&utm_source=create-next-app&utm_medium=appdir-template&utm_campaign=create-next-app"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          <Image
-            aria-hidden
-            src="/window.svg"
-            alt="Window icon"
-            width={16}
-            height={16}
-          />
-          Examples
-        </a>
-        <a
-          href="https://nextjs.org?utm_source=create-next-app&utm_medium=appdir-template&utm_campaign=create-next-app"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          <Image
-            aria-hidden
-            src="/globe.svg"
-            alt="Globe icon"
-            width={16}
-            height={16}
-          />
-          Go to nextjs.org →
-        </a>
-      </footer>
-    </div>
-  );
+  
+  return ( 
+    
+  <AppShell header={{height:60}}>
+     <AppShellHeader withBorder={false}>
+      <Paper shadow="md">
+      <Group py="sm" justify="space-around">
+      <Image src="\logo\AsaanDrive.png" h={20}  w="auto"/>
+      <Flex gap={25.37}>
+      <UnstyledButton>Home</UnstyledButton>
+      <UnstyledButton>How it Works</UnstyledButton>
+      <UnstyledButton>Browse Vehicles</UnstyledButton>
+      </Flex>
+      <Flex gap={18}>
+      <Button variant="outline" color="red" radius="md">Sign in</Button>
+      <Button  color="red" radius="md" >Sign up</Button>
+      </Flex>
+      </Group>
+      </Paper>
+     </AppShellHeader>
+     <AppShellMain>Main</AppShellMain>
+    </AppShell>  
+  
+   );
 }
